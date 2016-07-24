b61fe3e1906bce8ddaf8ac749cff030f5c8de788
Jon Borgonia <jon@gomagames.com>
2016-07-23 14:57:24 -1000

    add getSource helper function

--- routes/pokedex.js
+++ routes/pokedex.js
@@ -4,8 +4,12 @@ const express = require('express');
 const router = express.Router();
 const { client, index, type, MAX_SIZE } = require('../data/esClient');
 
+function getSource(doc){
+  return doc._source;
+}
+
 function getHitSource(res){
-  return res.hits.hits.map(r=>r._source);
+  return res.hits.hits.map(getSource);
 }
 
 router.get('/', ( req, res ) => client.search({ index, type, size : MAX_SIZE, match_all : {} })
